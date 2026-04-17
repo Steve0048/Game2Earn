d77aa5d8cb37059815bb264e3cebba28617bb432
Steve0048 <hounmenouidriss@gmail.com>
2026-04-17 21:28:21 +0100

    source map desactivation, terser minification security add

--- game2earn/vite.config.ts
+++ game2earn/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // 1. Désactive les Source Maps pour cacher ton code source
+    sourcemap: false,
+    
+    // 2. Minification avec Terser
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true, // Supprime les console.log
+        drop_debugger: true,
+      },
+      mangle: {
+        toplevel: true, // Mélange les noms de variables
+      },
+    },
+  },
 })
